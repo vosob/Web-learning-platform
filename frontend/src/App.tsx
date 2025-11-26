bceb7a994bf62e5bd339558ade43e4490ea5a365
vosob <osobskyi.vlad@gmail.com>
2025-11-26 17:06:39 +0200

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button";
+
+export const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">Welcome to the Web Learning Platform</h1>
+      <Button variant={"default"}>Click me</Button>
+    </div>
+  );
+};
